a04499c82450b9f46ba5c539aeecc045165c4797
Michael Contento <michaelcontento@gmail.com>
2014-12-28 23:15:26 +0100

    code added

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,46 @@
+extern crate time;
+use std::rand;
+
+static EPOCH:f64 = 946702800.0;
+static TIMESTAMP_SHIFT:uint = 23;
+static RANDOM_MAX_VALUE:u32 = 4194303;
+
+#[deriving(Copy)]
+pub struct Id {
+    pub timestamp: f64,
+    pub random_bits: u32,
+}
+
+pub fn new() -> u64 {
+    let now = time_as_float(time::get_time());
+    let timestamp = ((now - EPOCH) * 1000.0) as u64;
+    let random_bits = rand::random::<u32>() % RANDOM_MAX_VALUE;
+
+    (timestamp << TIMESTAMP_SHIFT) | (random_bits as u64)
+}
+
+pub fn parse(id:u64) -> Id {
+    let random_bits = (id as u32) & RANDOM_MAX_VALUE;
+    let mut timestamp = (id >> TIMESTAMP_SHIFT) as f64;
+    timestamp /= 1000.0;
+    timestamp += EPOCH;
+
+    Id { timestamp: timestamp, random_bits: random_bits }
+}
+
+fn time_as_float(spec:time::Timespec) -> f64 {
+    let mut nsec = spec.nsec as f64;
+    while nsec > 1.0 {
+        nsec /= 10.0;
+    }
+
+    (spec.sec as f64) + nsec
+}
+
 #[test]
-fn it_works() {
+fn test_time_as_float() {
+    let input = time::Timespec { sec: 12, nsec: 34};
+    let output = time_as_float(input);
+
+    assert_eq!(output, 12.34);
 }
---
+++ tests/lib.rs
@@ -0,0 +1,29 @@
+extern crate simpleflake;
+
+#[test]
+fn test_new() {
+    let id = simpleflake::new();
+    assert!(id > 0);
+}
+
+#[test]
+fn test_new_returns_different_values() {
+    let id_a = simpleflake::new();
+    let id_b = simpleflake::new();
+    assert!(id_a != id_b);
+}
+
+#[test]
+fn test_length_of_generated_ids() {
+    let id = simpleflake::new();
+    assert!(id > 1000000000000000000);
+}
+
+#[test]
+fn test_parse() {
+    let id = 3878068333444056242;
+    let parts = simpleflake::parse(id);
+
+    assert_eq!(parts.timestamp, 1409004570.859);
+    assert_eq!(parts.random_bits, 2081970);
+}
